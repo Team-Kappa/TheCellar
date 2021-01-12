0c32a8d640e22a2f673485975393b7e47a9e3579
Cwlowe <calvinwolfelowe@gmail.com>
2021-01-12 14:35:04 -0800

    FE: Added Homepage with connection to store BE: Added store for products singleWine and allWine

--- client/app.js
+++ client/app.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
-import {Navbar} from './components'
+import {Navbar, Homepage} from './components'
 import Routes from './routes'
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Homepage />
       <Routes />
     </div>
   )
