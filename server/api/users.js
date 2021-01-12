afbfdf9b69fe8274f8940af5985fe6b90da6d6f5
Matthew Leng <mleng89@gmail.com>
2021-01-12 16:45:33 -0500

    BE: Added User and Product routes

--- server/api/users.js
+++ server/api/users.js
@@ -1,17 +1,31 @@
 const router = require('express').Router()
 const {User} = require('../db/models')
-module.exports = router
 
+//GET ALL USER
 router.get('/', async (req, res, next) => {
   try {
     const users = await User.findAll({
       // explicitly select only the id and email fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
-      attributes: ['id', 'email']
+      attributes: ['id', 'email', 'username', 'cartItems']
     })
     res.json(users)
   } catch (err) {
     next(err)
   }
 })
+
+//GET SINGLE USER
+router.get('/:userId', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.userId, {
+      attributes: ['id', 'email', 'username', 'cartItems']
+    })
+    res.status(200).json(user)
+  } catch (error) {
+    next(error)
+  }
+})
+
+module.exports = router
