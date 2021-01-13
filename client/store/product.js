9e3b8b33cad5ce7db30ee9d3a17416877ea499ec
Matthew Leng <mleng89@gmail.com>
2021-01-13 15:31:10 -0500

    FE: Fixed routing and state for AllWine and SingleWineView

--- client/store/product.js
+++ client/store/product.js
@@ -50,9 +50,9 @@ export const fetchSingleWines = id => async dispatch => {
 export default function(state = defaultWine, action) {
   switch (action.type) {
     case SET_WINE:
-      return action.wines
+      return {...state, wines: action.wines}
     case SET_SINGLEWINE:
-      return action.singleWine
+      return {...state, singleWine: action.singleWine}
     default:
       return state
   }
