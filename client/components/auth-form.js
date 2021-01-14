91af31f0c9b7097106d0dbc5f21d3db828e9e502
Cwlowe <calvinwolfelowe@gmail.com>
2021-01-13 22:10:58 -0800

    FE: Updated form to Material UI, hooked up backend. Working: Signup username issue.

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -2,35 +2,96 @@ import React from 'react'
 import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
 import {auth} from '../store'
+//Material UI
+import {
+  FormControl,
+  TextField,
+  Container,
+  InputLabel,
+  Input,
+  FormHelperText
+} from '@material-ui/core'
+import {makeStyles} from '@material-ui/core/styles'
+
+const useStyles = makeStyles(() => ({
+  root: {
+    width: '100vh',
+    alignContent: 'center',
+    marginTop: '5%',
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    fontFamily: 'Lobster Two, cursive',
+    color: '#5b0e2d'
+  },
+  form: {
+    backgroundColor: 'white',
+    padding: '5%',
+    borderRadius: '5%',
+    margin: '5%'
+  }
+}))
 
 /**
  * COMPONENT
  */
 const AuthForm = props => {
   const {name, displayName, handleSubmit, error} = props
-
+  const classes = useStyles()
+  const [state, setState] = React.useState({
+    personName: '',
+    email: '',
+    password: ''
+  })
+  const handleChange = event => {
+    setState({...state, [event.target.id]: event.target.value})
+  }
+  const errorName = state.personName.length === 0
+  const errorPassword = state.password.length === 0
+  const errorEmail = state.email.length === 0
   return (
-    <div>
-      <form onSubmit={handleSubmit} name={name}>
-        <div>
-          <label htmlFor="email">
-            <small>Email</small>
-          </label>
-          <input name="email" type="text" />
-        </div>
-        <div>
-          <label htmlFor="password">
-            <small>Password</small>
-          </label>
-          <input name="password" type="password" />
-        </div>
-        <div>
-          <button type="submit">{displayName}</button>
-        </div>
-        {error && error.response && <div> {error.response.data} </div>}
-      </form>
+    <Container className={classes.root} maxWidth="xs">
+      <h1>{displayName}</h1>
+      <Container maxWidth="xs">
+        <form className={classes.form} onSubmit={handleSubmit} name={name}>
+          <FormControl required error={errorName}>
+            <TextField
+              label="Name"
+              id="personName"
+              value={state.personName}
+              onChange={handleChange}
+              helperText="Name field is required"
+              variant="outlined"
+            />
+          </FormControl>
+          <FormControl required error={errorPassword}>
+            <TextField
+              label="Password"
+              id="password"
+              value={state.password}
+              onChange={handleChange}
+              helperText="Password field is required"
+              variant="outlined"
+            />
+          </FormControl>
+          <FormControl required error={errorEmail}>
+            <TextField
+              label="Email"
+              id="email"
+              value={state.email}
+              onChange={handleChange}
+              helperText="Email field is required"
+              variant="outlined"
+            />
+          </FormControl>
+          <div>
+            <button type="submit">{displayName}</button>
+          </div>
+          {error && error.response && <div> {error.response.data} </div>}
+        </form>
+      </Container>
       <a href="/auth/google">{displayName} with Google</a>
-    </div>
+    </Container>
   )
 }
 
@@ -59,7 +120,7 @@ const mapSignup = state => {
 
 const mapDispatch = dispatch => {
   return {
-    handleSubmit(evt) {
+    handleSubmit: evt => {
       evt.preventDefault()
       const formName = evt.target.name
       const email = evt.target.email.value
--- client/store/user.js
+++ client/store/user.js
@@ -34,6 +34,7 @@ export const auth = (email, password, method) => async dispatch => {
   let res
   try {
     res = await axios.post(`/auth/${method}`, {email, password})
+    console.log(method)
   } catch (authError) {
     return dispatch(getUser({error: authError}))
   }
