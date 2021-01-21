9ce8371773bfebdcec9acd32a13e2f7f6c754970
Cwlowe <calvinwolfelowe@gmail.com>
2021-01-20 23:07:54 -0800

    FINAL CHECK: Looked at final items that needed to be addded. Added security to post and get requests for user and wine.

--- client/components/404.js
+++ client/components/404.js
@@ -1,11 +1,56 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
 
-const NotFound = () => (
-  <div>
-    <h1>404 - Not Found!</h1>
-    <Link to="/">Go Home</Link>
-  </div>
-)
+//Material UI
+import {
+  FormControl,
+  TextField,
+  Container,
+  Button,
+  Link
+} from '@material-ui/core'
+import {makeStyles} from '@material-ui/core/styles'
+
+//404 form styles
+const useStyles = makeStyles(() => ({
+  root: {
+    width: '100vh',
+    alignContent: 'center',
+    marginTop: '5%',
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    fontFamily: 'Lobster Two, cursive',
+    color: '#5b0e2d'
+  },
+  form: {
+    backgroundColor: 'white',
+    padding: '5%',
+    borderRadius: '5%',
+    margin: '5%',
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column'
+  },
+  formElm: {
+    paddingBottom: '2%'
+  },
+  button: {
+    backgroundColor: '#5b0e2d',
+    color: '#ffa781',
+    marginBottom: '5%'
+  }
+}))
+const NotFound = () => {
+  const classes = useStyles()
+  return (
+    <Container className={classes.root} maxWidth="xs">
+      <h1>404 - Not Found!</h1>
+      <p>
+        Sorry the page you are looking for is not here please go back to the{' '}
+        <Link href="/">Homepage</Link>
+      </p>
+    </Container>
+  )
+}
 
 export default NotFound
